d26a90b875b5b89e321ebd659e3a46be60db0c68
Diego Santos <diegosferreiradasilva@outlook.com>
2025-12-20 19:28:11 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
+const song = document.getElementById('audio');
+const play = document.getElementById('play');
+const cover = document.getElementById('cover');
+
+const Playlist = [AEncruzilhada, AdãoeEva, AgoraEstouSofrendo, AindaTeAmo, Amanhecer, AmordaMinhaVida, Armadilha, BabyDoll, BandidodoAmor, DeclaraçãodeAmor, ÉLindaNossaHistória, FaçoChover, Longe, Mágica, MorrendodeDesejo, PorAmor, Refém-Cobertor, Saideira-EmMilPedaços, TemMaisAlguém, VerdadeiroAmor]
